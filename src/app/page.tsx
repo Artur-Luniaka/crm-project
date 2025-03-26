e14b12c350edfe03c9485b234368ca45bcd582b3
Artur Luniaka <artur01049718@gmail.com>
2025-03-26 10:02:26 +0100

    added tailwind to project/applied for test component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
-import ActiveLabel from "./components/ActiveLabel";
+import StatusLabel, { Status } from "./components/StatusLabel";
 
 export default function Home() {
   return (
     <main>
       <h1>Home Page</h1>
-      <ActiveLabel>Active</ActiveLabel>
+      <StatusLabel status={Status.Active}>Active</StatusLabel>
+      <StatusLabel status={Status.NotActive}>NotActive</StatusLabel>
+      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
+      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
     </main>
   );
 }
